c6cb033920bd33ffe3558b07e05be42c0b2cb623
jarekbutek <jarekbutek@gmail.com>
2016-05-12 12:57:13 +0200

    usunięcie niepotrzebnych plików

--- src/lunarGraphics/Scene.java
+++ src/lunarGraphics/Scene.java
@@ -20,110 +20,39 @@ import lunarPlayer.Player;
  *
  * @author jarek
  */
-public class Scene extends Canvas {
-    JFrame frame = new JFrame("LunarLander");
-    JPanel panel;
-    Dimension minDim, preferredDim;
-    BufferStrategy bufferStrategy;
-    
-    
-	/** Obiekt klasy @class Level która przechowuje wszystkie informacje związane z danym poziomem */
-	Level level;
+public class Scene
+{
+    /**  Kontekst graficzny */
+    Graphics2D g2d; 
+       /** Obiekt klasy @class Level która przechowuje wszystkie informacje związane z danym poziomem */
+    Level level;
 	/** Obiekt @class Player, która przechowuje wszystkie informacje związane z danym graczem */
 
-	Player player;
+    Player player;
+
     public Scene()
     {
-        loadProperties("window.properties");
-        frame.setMinimumSize(minDim);
-        frame.setPreferredSize(preferredDim);
-        
-        panel = (JPanel)frame.getContentPane();
-        panel.add(this);
-        panel.setLayout(null);
-        Thread graphicThread = new Thread(new GraphicLoop(true, this));
-            graphicThread.start();
-        frame.pack();
-        frame.setVisible(true);
+        level = new Level();
+        level.loadLevel("map.properties");
+        player = new Player();
+        player.loadPlayer("player.properties");
     }
-    public void bufferedPaint()
-    {
-        
-    }
-    public void addNotify()
-    {
-        super.addNotify();
-        createBufferStrategy(2);
-        bufferStrategy = getBufferStrategy();
-    }
-    @Override
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D)bufferStrategy.getDrawGraphics();
-        super.paint(g2d);
-        
-        level.getMap().paintMap(g2d, getSize());
+
+       public void updateScene(Graphics2D g2d, Dimension size, Dimension preferredSize)
+       {
+        level.getMap().paintMap(g2d, size);
 		
-	double scaleX = (double)getWidth()/(double)getPreferredSize().getWidth();
-	double scaleY = (double)getHeight()/(double)getPreferredSize().getHeight();
+	double scaleX = (double)size.width/(double)preferredSize.getWidth();
+	double scaleY = (double)size.height/(double)preferredSize.getHeight();
 	
-	g2d.drawImage(player.getImage(), (int)(player.getX()*getWidth()),(int)(player.getY()*getHeight()) ,(int)(player.getImage().getWidth()*scaleX),(int)(player.getImage().getHeight()*scaleY),null);
+	g2d.drawImage(player.getImage(), (int)(player.getX()*size.width),(int)(player.getY()*size.height) ,(int)(player.getImage().getWidth()*scaleX),(int)(player.getImage().getHeight()*scaleY),null);
 	g2d.setColor(Color.white);
-	g2d.drawString("x: "+player.getX()*640, 0, (int)(getHeight()*0.05));
-	g2d.drawString("y: "+player.getY()*480, 0, (int)(getHeight()*0.1));
-	g2d.drawString("vX: "+player.getvX()*640, 0, (int)(getHeight()*0.15));
-	g2d.drawString("vY: "+player.getvY()*480, 0, (int)(getHeight()*0.2));
-	g2d.drawString("g: "+level.getGravity(), 0, (int)(getHeight()*0.25));
-	g2d.drawString("Fuel Level: "+player.getFuelLevel(), (int)(getWidth()-100), (int)(getHeight()*0.05));
-	g2d.drawString("Time: 0:00", (int)(getWidth()-100), (int)(getHeight()*0.1));
-                
-        bufferStrategy.show();
+	g2d.drawString("x: "+player.getX()*640, 0, (int)(size.height*0.05));
+	g2d.drawString("y: "+player.getY()*480, 0, (int)(size.height*0.1));
+	g2d.drawString("vX: "+player.getvX()*640, 0, (int)(size.height*0.15));
+	g2d.drawString("vY: "+player.getvY()*480, 0, (int)(size.height*0.2));
+	g2d.drawString("g: "+level.getGravity(), 0, (int)(size.height*0.25));
+	g2d.drawString("Fuel Level: "+player.getFuelLevel(), (int)(size.width-100), (int)(size.height*0.05));
+	g2d.drawString("Time: 0:00", (int)(size.width-100), (int)(size.height*0.1));
     }
-    
-    private void loadProperties(String filename)
-	{
-		try 
-		{
-			InputStream is = new FileInputStream(filename);
-			Properties properties = new Properties();
-			
-			properties.load(is);
-			int minResX = Integer.parseInt(properties.getProperty("minimumResX"));
-			int minResY = Integer.parseInt(properties.getProperty("minimumResY"));
-			minDim = new Dimension(minResX, minResY);
-			
-			int preferredResX = Integer.parseInt(properties.getProperty("preferredResX"));
-			int preferredResY = Integer.parseInt(properties.getProperty("preferredResY"));
-			preferredDim = new Dimension(preferredResX, preferredResY);
-				
-		}
-		catch (FileNotFoundException e)
-		{
-			System.out.println("Blad wczytywania pliku - ustawienia okna: " + filename);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		
-	}
-    private class GraphicLoop implements Runnable {
-
-	boolean isRunning;
-	Canvas panel;
-	public GraphicLoop(boolean isRunning, Canvas canvas)
-	{
-		this.isRunning = isRunning;
-		panel = canvas;
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		while(isRunning)
-		{
-                    repaint();
-		}
-	}
-
-        }
 }
